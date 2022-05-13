0650500d8c4f53fdec44ee2690ca347d7389a432
anggirmyni <anggiramayani123@gmail.com>
2022-05-13 14:13:24 +0700

    lanjutann

--- resources/views/prodi/create.blade.php
+++ resources/views/prodi/create.blade.php
@@ -5,31 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Prodi</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class = "container">
-        <div class ="row pt-4">
-            <div class ="col">
+    <div class="container">
+        <div class="row pt-4">
+            <div class="col">
                 <h2>Form Prodi</h2>
-                
-                @if (session()->has('info'))
-                <div class="alert alert-success">
-                    {{ session()->get('info') }}
-                </div>
-                <form action="{{url('prodi/store')}}" method ="post" enctype="multipart/form-data">
-                    <div class ="form-group">
-                        <label for="nama">Nama</label>     
-                        <input type="text" name="nama" id="nama" class="form-control" value="{{ old('nama') }}">
-                        @error('nama')
-                            <div class ="text-danger"> {{ $message }} </div>
-                        @enderror
+                <form action="{{ url('prodi/store') }}" method="post" enctype="multipart/form-data">
+                    @csrf   
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" name="nama" id="nama" class="form-control">
                     </div>
-                    <div class="form-group"> 
-                        <label for="foto">Gambar/Logo</label>
-                        <input type="file" name="foto" class="form-control">
+                    <div class="form-group">
+                    <label for="foto">Gambar/Logo</label>
+                    <input type="file" name="foto" class="form-control">
                     </div>
-                    <button type ="submit" class ="btn btn-primary mt-2">Simpan</button>
+                    <button type="submit" class="btn btn-primary mt-2">Simpan</button>
                 </form>
             </div>
         </div>
--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,48 +1,51 @@
+{{-- @include("layout.header", ['title' => "Halaman Program Studi"]) --}}
 
 @extends("layout.master")
-@section("title", "Halaman List Prodi")
+@section("title", "Halaman Program Studi")
 
 @section("content")
-<div class ="row pt-4">
-    <div class="col">
-        <h2>Prodi</h2>
-        <div class ="d-md-flex justify-content-md-end">
-        <a href="{{ route('prodi.create') }}" class="brn btn-primary">Tambah</a>
-    </div>
-    @if (session()->has('info'))
-        <div class="alert alert-success">
-            {{ session()->get('info')}}
+    <div class="row pt-4">
+        <div class="col">            
+            <h1>Program Studi</h1>
+            <div class="d-md-flex justify-content-md-end">
+                <a href="{{ route('prodi.create') }}" class="btn btn-primary">Tambah</a>
+            </div>
+            @if (session()->has('info'))
+                <div class="alert alert-success">
+                    {{ session()->get('info')}}
+                </div>
+            @endif
+            <div>
+                <table class="table table-striped table-hover">
+                    <thread>
+                        <tr>
+                            <th>Nama</th>
+                            <th>Fakultas</a>
+                            <th>Logo</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                    @foreach($prodi as $item)
+                        <tr>
+                            <td> {{ $item->nama }} </td>
+                            <td> {{ $item->fakultas->nama }} </td>
+                            <td> <img src="{{ asset('storage/'.$item->foto) }}" width="100"> </td>
+                            <td>
+                                <form action="{{ route('prodi.destroy', ['prodi' => $item->id]) }}" method="POST">
+                                    <a href="{{ url('/prodi/'.$item->id) }}" class="btn btn-warning">Detail</a>
+                                    <a href="{{ url('/prodi/'.$item->id.'/edit') }}" class="btn btn-info">Ubah</a>
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        {{-- <li>{{ $item->nama }} {{ $item->fakultas->nama }}</li> --}}
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    @endif
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>Nama</th>
-                <th>Fakutltas</th>
-                <th>Logo</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($prodis as $item)
-            <tr>
-                <td> {{ $item->nama }} </td>
-                <td> {{ $item->fakultas->nama }} </td>
-                <td> <img src = " {{ $asset('storage/'.$item->foto)}} " width="100"></td>
-                <td>
-                    <a href="{{ url('prodi/'.$item->id) }}" class="btn btn-warning">Detail</a>
-                    <a href="{{ url('prodi/'.$item->id.' /edit) }}" class="btn btn-info">Ubah</a>
-                    <form action="{{ route('prodi' => $item->id]) }}" 
-                    method="POST">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" class="btn btn-danger">Hapus</button>
-                    </form>
-                 </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
-</div>
+    </div>
 @endsection
